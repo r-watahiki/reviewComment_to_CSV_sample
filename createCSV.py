dbc5af4baf598e485b22fc92d6fd0ab98b899804
綿引里沙 <58762936+r-watahiki@users.noreply.github.com>
2021-03-26 18:03:31 +0900

    Update createCSV.py

--- createCSV.py
+++ createCSV.py
@@ -6,11 +6,12 @@ import requests.packages.urllib3
 
 requests.packages.urllib3.disable_warnings()
 
-token = os.environ['token']
-repo = os.environ['repo']
+
     
 def main():
     os.environ['no_proxy'] = 'localhost'
+    token = os.environ['token']
+    repo = os.environ['repo']
     url = 'https://api.github.com/repos/' + repo + '/pulls?access_token='+ token +'&state=all'
     response = requests.get(url, verify=False)
     json_dict = json.loads(response.text)
