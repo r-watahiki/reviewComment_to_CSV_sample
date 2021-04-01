74777d62097ef4f856f134ecfc0a8129be048447
綿引里沙 <58762936+r-watahiki@users.noreply.github.com>
2021-04-01 16:41:53 +0900

    Update createCSV.py

--- createCSV.py
+++ createCSV.py
@@ -28,7 +28,7 @@ def main():
        rv_dict = json.loads(rv_response.text)
                
        for review in rv_dict:
-          rv_se = pd.Series([cnt,review['id'],review['body'],review['user']['login'],review['submitted_at'],review['html_url']], index=cm_df.columns)
+          rv_se = pd.Series([cnt,review['id'],review['body'],review['user']['login'],review['submitted_at'],review['html_url']], index=rv_df.columns)
           rv_df = rv_df.append( rv_se, ignore_index=True)
        
        issuecm_url = 'https://api.github.com/repos/'+ repo +'/issues/' + cnt + '/comments?access_token=' + token # PRConversationで残したコメント
@@ -36,7 +36,7 @@ def main():
        issuecm_dict = json.loads(issuecm_response.text)
                
        for issuecm in issuecm_dict:
-          issuecm_se = pd.Series([cnt,issue['id'],issuecm['body'],issuecm['user']['login'],issuecm['updated_at'],issuecm['html_url'],""], index=issuecm_df.columns)
+          issuecm_se = pd.Series([cnt,issue['id'],issuecm['body'],issuecm['user']['login'],issuecm['updated_at'],issuecm['html_url']], index=cm_df.columns)
           rv_df = rv_df.append( issuecm_se, ignore_index=True)
           
        cm_url = 'https://api.github.com/repos/'+ repo + '/pulls/' + cnt + '/comments?access_token=' + token # PRのFileChangedでAddsinglecomment/Startareviewで残したコメント
